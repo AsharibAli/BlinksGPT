ce48ba887642d6c7dd62d6e4edefa94b11f65334
Asharib Ali <asharibarain4@gmail.com>
2024-08-19 16:55:44 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import WalletProvider from './providers/WalletProvider';
+import WalletProvider from '../providers/WalletProvider';
 
 const inter = Inter({ subsets: ["latin"] });
 
