39203a4169627e5856738ebec2475606522a9841
Asharib Ali <asharibarain4@gmail.com>
2024-08-20 23:48:14 +0500

    update

--- components/UnlockBlink.tsx
+++ components/UnlockBlink.tsx
@@ -138,9 +138,9 @@ export default function UnlockBlinks() {
 
   return (
    <div className="flex flex-col items-center justify-center h-screen">
-    <h1 className="text-2xl mb-4">💗 Welcome {username ? username : "User"} to the BlinksGPT 🤖</h1>
+    <h1 className="text-2xl mb-4">💗 Welcome {<strong>${username}</strong>  ? username : "User"} to the BlinksGPT 🤖</h1>
     {address ? (
-      <strong className="text-sm mb-4 text-gray-500">Connected Wallet Address: {address}</strong>
+      <p className="text-sm mb-4 text-gray-500"><strong>Connected Wallet Address:</strong>{address}</p>
     ) : (
       <p className="text-sm mb-4 text-red-500">Please connect your wallet to proceed.</p>
     )}
